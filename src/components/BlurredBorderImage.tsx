dc90aa7ef3bfc923a38388d1af5faef8d9c14169
Camille Bouzerand <bcamille99@gmail.com>
2024-07-22 16:53:27 +0100

    [FIX] blurred border image correct now

--- src/components/BlurredBorderImage.tsx
+++ src/components/BlurredBorderImage.tsx
@@ -1,31 +1,75 @@
-import { Box, Image } from "@chakra-ui/react";
+'use client'
+
+import { Box, Image, useColorModeValue, useTheme } from "@chakra-ui/react";
 
 function BlurredBorderImage() {
+  const theme = useTheme();
+  const backgroundColor = useColorModeValue(theme.colors.lightBackground, theme.colors.darkBackground);
+
+  const gradientTop = `linear-gradient(to bottom, ${backgroundColor}, #0d060300)`;
+  const gradientRight = `linear-gradient(to left, ${backgroundColor}, #0d060300)`;
+  const gradientBottom = `linear-gradient(to top, ${backgroundColor}, #0d060300)`;
+  const gradientLeft = `linear-gradient(to right, ${backgroundColor}, #0d060300)`;
+
   return (
     <Box position="relative" display="inline-block">
-      {/* Blurred border layer */}
+      {/* Image layer */}
+      <Image
+        src="forest-4.jpg"
+        alt="Forest"
+        borderRadius="md"
+        zIndex={1}
+        position="relative"
+        objectFit="cover"
+      />
+      {/* Top border */}
       <Box
         position="absolute"
         top="0"
         left="0"
         right="0"
-        bottom="0"
-        borderRadius="md"
-        backdropFilter="blur(10px)"
-        bg="rgba(255, 255, 255, 0.2)" // semi-transparent background
-        zIndex={0}
-        filter="blur(10px)"
-        boxSize="full"
+        height="40px"
+        bg={gradientTop}
+        zIndex={2}
+        borderTopLeftRadius="md"
+        borderTopRightRadius="md"
       />
-      {/* Image layer */}
-      <Box position="relative" zIndex={1} boxSize="full">
-        <Image
-          src="forest-4.jpg"
-          alt="Forest"
-          borderRadius="md"
-          boxSize="full"
-        />
-      </Box>
+      {/* Right border */}
+      <Box
+        position="absolute"
+        top="0"
+        right="0"
+        bottom="0"
+        width="40px"
+        bg={gradientRight}
+        zIndex={2}
+        borderTopRightRadius="md"
+        borderBottomRightRadius="md"
+      />
+      {/* Bottom border */}
+      <Box
+        position="absolute"
+        left="0"
+        right="0"
+        bottom="0"
+        height="40px"
+        bg={gradientBottom}
+        zIndex={2}
+        borderBottomLeftRadius="md"
+        borderBottomRightRadius="md"
+      />
+      {/* Left border */}
+      <Box
+        position="absolute"
+        top="0"
+        left="0"
+        bottom="0"
+        width="40px"
+        bg={gradientLeft}
+        zIndex={2}
+        borderTopLeftRadius="md"
+        borderBottomLeftRadius="md"
+      />
     </Box>
   );
 }
